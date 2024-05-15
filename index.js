120c16a83c6d3d9abf9dc2e1bc043c73b5c8cc75
MadhanAB <madhan04pg@gmail.com>
2024-05-15 20:09:08 +0530

    cors url changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000","http://yourliveserver.com"],
+  origin: ["http://localhost:3000","https://bejewelled-rabanadas-734881.netlify.app"],
   credentials:true,
 }));
 
